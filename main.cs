07c88ffb121ec818af87568662098bb4b51a70d9
Joaquin <joaquinnicita@gmail.com>
2024-04-13 15:30:24 -0300

    commit

--- main.cs
+++ main.cs
@@ -7,36 +7,44 @@ class Program {
       Console.WriteLine("Ingrese la frase a cifrar");
 
       string msj = Console.ReadLine();
+    
+      Console.WriteLine("Ingrese la distancia");
+      int distancia = int.Parse(Console.ReadLine());    
+    
       while (string.IsNullOrEmpty(msj))
       {
           Console.WriteLine("Ingrese un mensaje no vacio");
           msj = Console.ReadLine();
       }
       Program program = new Program();    
-      string res = program.cifrar(msj);
+      string res = program.cifrar(msj, distancia);
       Console.WriteLine(res);
-
-    Console.WriteLine("Ingrese la frase a descifrar");
-
-    string msj1 = Console.ReadLine();
-    while (string.IsNullOrEmpty(msj))
+    
+      Console.WriteLine("Ingrese la frase a descifrar");
+      string msj1 = Console.ReadLine();
+    
+      Console.WriteLine("Ingrese la distancia");
+    
+      int distancia1 = int.Parse(Console.ReadLine());   
+      while (string.IsNullOrEmpty(msj))
     {
         Console.WriteLine("Ingrese un mensaje no vacio");
         msj1 = Console.ReadLine();
     }    
-    string res1 = program.descifrar(msj1);
+    string res1 = program.descifrar(msj1, distancia1);
     Console.WriteLine(res1);
+    
     
   }
 
-  public string cifrar(string msj) {
+  public string cifrar(string msj, int distancia) {
     string cifrado = "";
     foreach (char c in msj)
       {
             int pos = alfabeto.IndexOf(c);
             if (pos != -1)
             {
-                pos = (pos + 7) % alfabeto.Length;
+                pos = (pos + distancia) % alfabeto.Length;
                 cifrado += alfabeto[pos];
             }
             else
@@ -47,14 +55,14 @@ class Program {
     return cifrado;
   }
 
-  public string descifrar (string msj1){
+  public string descifrar (string msj1, int distancia1){
     string descifrado = "";
       foreach (char c in msj1)
       {
           int pos = alfabeto.IndexOf(c);
             if (pos != -1)
             {
-                pos = (pos - 7) % alfabeto.Length;
+                pos = (pos - distancia1) % alfabeto.Length;
                 descifrado += alfabeto[pos];
             }
             else
@@ -65,70 +73,4 @@ class Program {
       return descifrado;
   }
 }
-using System;
 
-class Program {
-  
-  static string alfabeto = "abcdefghijklmñnopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ1234567890_-+,#$%&/()=¿?¡!|,.;:{}[]";
-  public static void Main(){
-      Console.WriteLine("Ingrese la frase a cifrar");
-
-      string msj = Console.ReadLine();
-      while (string.IsNullOrEmpty(msj))
-      {
-          Console.WriteLine("Ingrese un mensaje no vacio");
-          msj = Console.ReadLine();
-      }
-      Program program = new Program();    
-      string res = program.cifrar(msj);
-      Console.WriteLine(res);
-
-    Console.WriteLine("Ingrese la frase a descifrar");
-
-    string msj1 = Console.ReadLine();
-    while (string.IsNullOrEmpty(msj))
-    {
-        Console.WriteLine("Ingrese un mensaje no vacio");
-        msj1 = Console.ReadLine();
-    }    
-    string res1 = program.descifrar(msj1);
-    Console.WriteLine(res1);
-    
-  }
-
-  public string cifrar(string msj) {
-    string cifrado = "";
-    foreach (char c in msj)
-      {
-            int pos = alfabeto.IndexOf(c);
-            if (pos != -1)
-            {
-                pos = (pos + 7) % alfabeto.Length;
-                cifrado += alfabeto[pos];
-            }
-            else
-            {
-                cifrado += c;
-            }
-        }
-    return cifrado;
-  }
-
-  public string descifrar (string msj1){
-    string descifrado = "";
-      foreach (char c in msj1)
-      {
-          int pos = alfabeto.IndexOf(c);
-            if (pos != -1)
-            {
-                pos = (pos - 7) % alfabeto.Length;
-                descifrado += alfabeto[pos];
-            }
-            else
-            {
-                descifrado += c;
-            }
-      }
-      return descifrado;
-  }
-}
